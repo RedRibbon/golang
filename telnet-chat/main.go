eca45842e4806b04b4e89f4d61212dd80caf6379
YongHun Byun <yonghunbyun@gmail.com>
2015-02-04 23:39:56 +0900

    Add new client registration feature etc.
    - a way to register new clients
    - a way to unregister clients that have disconnected
    - messages sent by a client shall be broadcast to all clients

--- telnet-chat/main.go
+++ telnet-chat/main.go
@@ -1,50 +1,117 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io"
 	"log"
 	"net"
+	"os"
+	"strings"
 )
+
+type Client struct {
+	conn     net.Conn
+	nickname string
+	ch       chan string
+}
 
 func main() {
 	ln, err := net.Listen("tcp", ":6000")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	msgchan := make(chan string)
-	go printMessages(msgchan)
+	addchan := make(chan Client)
+	rmchan := make(chan Client)
+
+	go handleMessages(msgchan, addchan, rmchan)
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Println(err)
+			fmt.Println(err)
 			continue
 		}
-		go handleConnection(conn, msgchan)
+
+		go handleConnection(conn, msgchan, addchan, rmchan)
 	}
 }
 
-func printMessages(msgchan <-chan string) {
-	for msg := range msgchan {
-		log.Printf("new message: %s", msg)
+func (c Client) ReadLinesInto(ch chan<- string) {
+	bufc := bufio.NewReader(c.conn)
+	for {
+		line, err := bufc.ReadString('\n')
+		if err != nil {
+			break
+		}
+		ch <- fmt.Sprintf("%s: %s", c.nickname, line)
 	}
 }
 
-func handleConnection(c net.Conn, msgchan chan<- string) {
-	buf := make([]byte, 4096)
+func (c Client) WriteLinesFrom(ch <-chan string) {
+	for msg := range ch {
+		_, err := io.WriteString(c.conn, msg)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func promptNick(c net.Conn, bufc *bufio.Reader) string {
+	io.WriteString(c, "\033[1;30;41mWelcome to the fancy demo chat!\033[0m\n")
+	io.WriteString(c, "What is your nick? ")
+	nick, _, _ := bufc.ReadLine()
+	return string(nick)
+}
+
+func handleConnection(c net.Conn, msgchan chan<- string, addchan chan<- Client, rmchan chan<- Client) {
+	bufc := bufio.NewReader(c)
+	defer c.Close()
+	client := Client{
+		conn:     c,
+		nickname: promptNick(c, bufc),
+		ch:       make(chan string),
+	}
+	if strings.TrimSpace(client.nickname) == "" {
+		io.WriteString(c, "Invalid Username\n")
+		return
+	}
+
+	// Register user
+	addchan <- client
+	defer func() {
+		msgchan <- fmt.Sprintf("User %s left the chat room.\n", client.nickname)
+		log.Printf("Connection from %v closed.\n", c.RemoteAddr())
+		rmchan <- client
+	}()
+	io.WriteString(c, fmt.Sprintf("Welcome, %s!\n\n", client.nickname))
+
+	msgchan <- fmt.Sprintf("New user %s has joined the chat room.\n", client.nickname)
+
+	// I/O
+	go client.ReadLinesInto(msgchan)
+	client.WriteLinesFrom(client.ch)
+}
+
+func handleMessages(msgchan <-chan string, addchan <-chan Client, rmchan <-chan Client) {
+	clients := make(map[net.Conn]chan<- string)
 
 	for {
-		n, err := c.Read(buf)
-		if err != nil || n == 0 {
-			c.Close()
-			break
-		}
-		msgchan <- string(buf[0:n])
-		n, err = c.Write(buf[0:n])
-		if err != nil {
-			c.Close()
-			break
+		select {
+		case msg := <-msgchan:
+			log.Printf("New message: %s", msg)
+			for _, ch := range clients {
+				go func(mch chan<- string) { mch <- "\033[1;33;40m" + msg + "\033[m" }(ch)
+			}
+		case client := <-addchan:
+			log.Printf("New client: %v\n", client.conn)
+			clients[client.conn] = client.ch
+		case client := <-rmchan:
+			log.Printf("Client disconnects: %v\n", client.conn)
+			delete(clients, client.conn)
 		}
 	}
-	log.Printf("Connection from %v closed.", c.RemoteAddr())
 }
